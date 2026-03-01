6539a5d65cc43a193435310cbe39d07db07dccae
mairamartinsk <mairamartinsk@gmail.com>
2026-03-01 16:51:09 +0100

    Add pure comments

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -11,7 +11,7 @@ $post = $post ?? [];
 <div class="post-nav">
   <div>
     <?php if (!empty($previous_post)): ?>
-      <p>⬅ Previous post<br>
+      <p>⇠ Previous post<br>
       <a class="pagination-links" href="/<?= e((string) ($previous_post['slug'] ?? '')) ?>">
         <?= e((string) ($previous_post['title'] ?? '')) ?>
       </a></p>
@@ -20,10 +20,19 @@ $post = $post ?? [];
 
   <div class="post-nav-next">
     <?php if (!empty($next_post)): ?>
-      <p>Next post ➡<br>
+      <p>Next post ⇢<br>
       <a class="pagination-links" href="/<?= e((string) ($next_post['slug'] ?? '')) ?>">
         <?= e((string) ($next_post['title'] ?? '')) ?>
       </a></p>
     <?php endif; ?>
   </div>
 </div>
+
+<div class="post-comments">
+<div class="mail">
+  <a class="button reply-button"href="mailto:{{ site_email }}?subject=Reply to: {{ page_title }}">✉️ Reply by email</a>
+  <p></p>
+</div>
+
+<div id="comments" data-post-slug="<?= $post['slug'] ?>"></div>
+<script src="https://comments.mairamartins.com/public/embed.js" defer></script>
