a6b7a88c0a9a33256e3b63632470f8dcb6837bad
mairamartinsk <mairamartinsk@gmail.com>
2026-03-01 16:55:52 +0100

    Add GoatCounter stats

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,6 @@
 <footer>
     <p>&copy; <?= e((new DateTimeImmutable('now', site_timezone_object($config)))->format('Y')) ?> <?= e($config['site_title']) ?></p>
 </footer>
+
+<script data-goatcounter="https://maira.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
