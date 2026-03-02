0a4b7ebe898c99f5ecea43a5daaa8d87da94df89
mairamartinsk <mairamartinsk@gmail.com>
2026-03-02 10:00:43 +0100

    Add webrings

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,19 @@
 <footer>
+<div class="rings">
+<div>
+    <a href="https://fediring.net/previous?host=mairamartins.com">←</a>
+    <a href="https://fediring.net/">Fediring</a>
+    <small><a href="https://fediring.net/random">(random)</a></small>
+    <a href="https://fediring.net/next?host=mairamartins.com">→</a>
+</div>
+<div>
+    <a href="https://xn--sr8hvo.ws/previous">←</a>
+    <a href="https://xn--sr8hvo.ws">IndieWeb Webring</a> 🕸💍
+    <small><a href="https://🕸💍.ws/random">(random)</a></small>
+    <a href="https://xn--sr8hvo.ws/next">→</a>
+</div>
+</div>
+
     <p>&copy; <?= e((new DateTimeImmutable('now', site_timezone_object($config)))->format('Y')) ?> <?= e($config['site_title']) ?></p>
 </footer>
 
