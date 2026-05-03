a0479113969a515b15a35633b629608ee412e7a4
mairamartinsk <github@mairamartins.com>
2026-05-03 13:43:17 +0200

    Improve css for layout notes

--- content/layouts/notes.php
+++ content/layouts/notes.php
@@ -1,4 +1,12 @@
-<article class="note">
+<article class="layout">
+    <div class="notes">
+    <?php if (isset($post['displayTitle']) && $post['displayTitle'] === '1'): ?>
+        <h1><?= e($post['title']) ?></h1>
+    <?php endif; ?>
     <?= render_markdown($post['content']) ?>
+    <?php if ($post['date']): ?>
+                    <p class="post-date"><svg class="icon" aria-hidden="true"><use href="#icon-calendar"></use></svg> <time datetime="<?= e(format_datetime_for_display((string) $post['date'], $config, 'c')) ?>"><?= e(format_post_date_for_display((string) $post['date'], $config)) ?></time></p>
+                <?php endif; ?>
+    </div>
     <?= render_post_navigation() ?>
 </article>
