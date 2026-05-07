f6b4dba559cd2bbcfed7aec64f4ea4049b4708d9
mairamartinsk <github@mairamartins.com>
2026-05-07 09:20:02 +0200

    Implement image grid collage

--- content/posts/2017-04-27-morocco-house.md
+++ content/posts/2017-04-27-morocco-house.md
@@ -6,13 +6,14 @@ status: published
 tags: [photos, travels]
 description: 
 layout: notes
+displayTitle: 
 ---
 
 Morocco is a place of contrasts. An unsuspecting door in a back alley opens up to a parallel universe of mosaics, carved wood and beauty. 🗺️
-
-![image](/content/images/2017-04-27/d147e4a8cf.jpg)
-
-<div class="collage">
-<img src="/content/images/2017-04-27/5c1bd65a9a.jpg">
-<img src="/content/images/2017-04-27/98e32fc008.jpg">
-</div>
+{{1
+![morocco 2018](/content/images/2017-04-27/d147e4a8cf.jpg)
+}}
+{{
+![morocco 2018](/content/images/2017-04-27/5c1bd65a9a.jpg)
+![morocco 2018](/content/images/2017-04-27/98e32fc008.jpg)
+}}
--- lib/ParsedownPureblog.php
+++ lib/ParsedownPureblog.php
@@ -14,19 +14,79 @@ class ParsedownPureblog extends ParsedownExtra
     protected function element(array $Element)
     {
         if (
-            isset($Element['name']) && $Element['name'] === 'p'
-            && isset($Element['handler']['function']) && $Element['handler']['function'] === 'lineElements'
-            && isset($Element['handler']['argument'])
+            isset($Element["name"]) &&
+            $Element["name"] === "p" &&
+            isset($Element["handler"]["function"]) &&
+            $Element["handler"]["function"] === "lineElements" &&
+            isset($Element["handler"]["argument"])
         ) {
-            $text = $Element['handler']['argument'];
-            $pattern = '/[ ]*{(' . $this->regexAttribute . '+)}[ ]*$/';
+            $text = $Element["handler"]["argument"];
+            $pattern = "/[ ]*{(" . $this->regexAttribute . '+)}[ ]*$/';
 
             if (preg_match($pattern, $text, $matches, PREG_OFFSET_CAPTURE)) {
-                $Element['attributes'] = $this->parseAttributeData($matches[1][0]);
-                $Element['handler']['argument'] = substr($text, 0, $matches[0][1]);
+                $Element["attributes"] = $this->parseAttributeData(
+                    $matches[1][0],
+                );
+                $Element["handler"]["argument"] = substr(
+                    $text,
+                    0,
+                    $matches[0][1],
+                );
             }
         }
 
         return parent::element($Element);
+    }
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->BlockTypes["{"][] = "CustomGrid";
+    }
+
+    protected function blockCustomGrid($Line)
+    {
+        if (
+            preg_match('/^[ ]*{{[ ]*(\d+)?[ ]*[ ]*$/', $Line["text"], $matches)
+        ) {
+            $columns = isset($matches[1]) ? $matches[1] : 2;
+
+            return [
+                "element" => [
+                    "name" => "div",
+                    "attributes" => [
+                        "class" => "layout-grid",
+                        "style" => "--grid-cols: $columns;",
+                    ],
+                ],
+            ];
+        }
+    }
+
+    protected function blockCustomGridContinue($Line, $Block)
+    {
+        if (isset($Block["complete"])) {
+            return;
+        }
+
+        if (preg_match('/^[ ]*}}[ ]*$/', $Line["text"])) {
+            $Block["complete"] = true;
+            return $Block;
+        }
+
+        $Block["lines"][] = $Line["text"];
+
+        return $Block;
+    }
+
+    protected function blockCustomGridComplete($Block)
+    {
+        if (isset($Block["lines"])) {
+            $innerMarkdown = implode("\n", $Block["lines"]);
+
+            $Block["element"]["rawHtml"] = parent::text($innerMarkdown);
+        }
+
+        return $Block;
     }
 }
